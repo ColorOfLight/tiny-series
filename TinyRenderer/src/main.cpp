6c6d3abcd98035eac1b76968f6b752ed11f5e693
Seongho <bluetosh7@gmail.com>
2024-06-30 18:01:39 +0900

    feat: add tgaimage header and cpp files

--- TinyRenderer/src/main.cpp
+++ TinyRenderer/src/main.cpp
@@ -1,9 +1,13 @@
-#include <iostream>
+#include "tgaimage.h"
 
-using namespace std;
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
 
-int main()
+int main(int argc, char **argv)
 {
-  cout << "Hello, World!" << endl;
+  TGAImage image(100, 100, TGAImage::RGB);
+  image.set(52, 41, red);
+  image.flip_vertically(); // I want to have the origin at the left bottom corner of the image
+  image.write_tga_file("output.tga");
   return 0;
 }
