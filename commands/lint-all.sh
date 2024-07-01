4e22d99a1d0dcae66a89063fa2568df21df72fd0
Seongho <bluetosh7@gmail.com>
2024-07-01 12:32:22 +0900

    feat: add filter to lint-all script

--- commands/lint-all.sh
+++ commands/lint-all.sh
@@ -1,2 +1,5 @@
-find ./TinyRenderer/include -name '*.h' -exec cpplint {} +
-find ./TinyRenderer/src -name '*.cpp' -exec cpplint {} +
+# Always perfer references over pointers
+CPPLINT_OPTIONS="--filter=-runtime/references"
+
+find ./TinyRenderer/include -name '*.h' -exec cpplint $CPPLINT_OPTIONS {} +
+find ./TinyRenderer/src -name '*.cpp' -exec cpplint $CPPLINT_OPTIONS {} +
