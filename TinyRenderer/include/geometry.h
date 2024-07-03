93fa49dbdfe509d72ed7125ed9319202adf09535
Seongho <bluetosh7@gmail.com>
2024-07-03 11:13:28 +0900

    TinyRenderer(fix): remove newline char from printing vectors

--- TinyRenderer/include/geometry.h
+++ TinyRenderer/include/geometry.h
@@ -102,13 +102,13 @@ typedef Vec3<int> Vec3i;
 
 template <class t>
 std::ostream &operator<<(std::ostream &s, const Vec2<t> &v) {
-  s << "(" << v.x << ", " << v.y << ")\n";
+  s << "(" << v.x << ", " << v.y << ")";
   return s;
 }
 
 template <class t>
 std::ostream &operator<<(std::ostream &s, const Vec3<t> &v) {
-  s << "(" << v.x << ", " << v.y << ", " << v.z << ")\n";
+  s << "(" << v.x << ", " << v.y << ", " << v.z << ")";
   return s;
 }
 
