c76cef3bb4eb0c974de1b558615314a57ad15526
Seongho <bluetosh7@gmail.com>
2024-07-28 10:54:03 +0900

    cpps(files): move geometry into common direc.

--- cpps/common/geometry/utils.h
+++ cpps/common/geometry/utils.h
@@ -27,6 +27,8 @@
 #include "./geometry/mat.h"
 #include "./geometry/vec.h"
 
+const float kEpsilon = 1e-5;
+
 inline Vec<3, float> GetNDC(const Vec<4, float>& vec) {
   if (vec[3] == 0) {
     throw std::runtime_error("GetNDC: Division by zero");
