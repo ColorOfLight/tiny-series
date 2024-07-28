981451e8f8b4bf625304af7463781d10f3b14171
Seongho <bluetosh7@gmail.com>
2024-07-28 14:45:26 +0900

    cpps(fix): fix Reflect's result direction

--- cpps/common/geometry/utils.h
+++ cpps/common/geometry/utils.h
@@ -39,7 +39,7 @@ inline Vec<3, float> GetNDC(const Vec<4, float>& vec) {
 
 inline Vec<3, float> Reflect(const Vec<3, float>& incident,
                              const Vec<3, float>& normal) {
-  return (incident - normal * 2 * (incident * normal)) * (-1);
+  return (incident - normal * 2 * (incident * normal));
 }
 
 inline bool IsParallel(const Vec<3, float>& a, const Vec<3, float>& b) {
