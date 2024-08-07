d21a9fed6851f21f1dfdf01928b9103dd62ac5c6
Seongho <bluetosh7@gmail.com>
2024-08-07 18:16:21 +0900

    page(feat): add onMenuClick props to Navigation

--- page/src/modules/Navigation.tsx
+++ page/src/modules/Navigation.tsx
@@ -5,21 +5,25 @@ import MenuIcon from "../components/MenuIcon";
 
 export interface NavigationProps extends ComponentPropsWithoutRef<"div"> {
   title: string;
+  onMenuClick?: React.EventHandler<React.MouseEvent<HTMLButtonElement>>;
 }
 
 const Navigation = memo(
   forwardRef<HTMLDivElement, NavigationProps>(
-    ({ className, title, ...restProps }, ref) => {
+    ({ className, title, onMenuClick, ...restProps }, ref) => {
       return (
         <div
-          className={clsx("flex items-center ml-m2", className)}
+          className={clsx(
+            "flex items-center ml-m2 p-3 border-b w-full",
+            className
+          )}
           {...restProps}
           ref={ref}
         >
-          <button className="p-2 border-0">
+          <button className="p-2 border-0" onClick={onMenuClick}>
             <MenuIcon className="fill-gray-500" />
           </button>
-          <nav className="text-gray-400 ml-1">
+          <nav className="text-gray-400 ml-1 text-sm">
             <ul className="flex gap-1">
               <li>Tiny Series</li>
               <li className="text-gray-700">{title}</li>
