323bb977e7d0f5c47de1104a782f4e906d153b92
Seongho <bluetosh7@gmail.com>
2024-08-07 20:30:07 +0900

    page(feat): add more effects to useNavigationPane

--- page/src/modules/NavigationPane.hook.ts
+++ page/src/modules/NavigationPane.hook.ts
@@ -1,10 +1,35 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
+
+// TODO: Replace with the real value
+const PANE_WIDTH_LIMIT = 640;
 
 const useNavigationPane = () => {
   const [isPaneOpen, setIsPaneOpen] = useState(false);
 
   const openPane = useCallback(() => setIsPaneOpen(true), []);
   const closePane = useCallback(() => setIsPaneOpen(false), []);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.innerWidth > PANE_WIDTH_LIMIT) {
+        closePane();
+      }
+    };
+
+    window.addEventListener("resize", handler);
+
+    return () => {
+      window.removeEventListener("resize", handler);
+    };
+  }, [closePane]);
+
+  useEffect(() => {
+    if (isPaneOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [isPaneOpen]);
 
   return {
     isPaneOpen,
