9bbfdbe6a49e6a6ded76382c3554eaa5951faf19
Seongho <bluetosh7@gmail.com>
2024-09-26 17:13:47 +0900

    fix: fix mistypo in lint-all.sh

--- cpps/commands/lint-all.sh
+++ cpps/commands/lint-all.sh
@@ -1,8 +1,8 @@
 # Always perfer references over pointers
 CPPLINT_OPTIONS="--filter=-runtime/references"
 
-find ./common -nam '*.h' -exec cpplint $CPPLINT_OPTIONS {} +
-find ./common -nam '*.cpp' -exec cpplint $CPPLINT_OPTIONS {} +
+find ./common -name '*.h' -exec cpplint $CPPLINT_OPTIONS {} +
+find ./common -name '*.cpp' -exec cpplint $CPPLINT_OPTIONS {} +
 find ./TinyRenderer/include -name '*.h' -exec cpplint $CPPLINT_OPTIONS {} +
 find ./TinyRenderer/src -name '*.cpp' -exec cpplint $CPPLINT_OPTIONS {} +
 find ./TinyRayTracer/include -name '*.h' -exec cpplint $CPPLINT_OPTIONS {} +
